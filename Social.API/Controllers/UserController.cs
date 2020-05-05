0db59925f2a2c3703467f4b0ecbbd0a3c5f95735
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2020-05-05 11:25:49 +0200

    Created UserController

---
+++ Social.API/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Social.API.Services;
+using AutoMapper;
+using Social.API.Dtos;
+using System.Linq;
+using Social.API.Models.Fake;
+
+namespace Social.API.Controllers
+{
+    [Route("api/v1.0/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _repo;
+        private readonly IMapper _mapper;
+        public UserController(IUserRepository repo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repo = repo;
+        }
+    }
+}
