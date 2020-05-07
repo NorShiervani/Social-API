7cde922127b722f0d7b3dd2e94c328cc96c11c46
robodostopif <boomeraron@gmail.com>
2020-05-07 10:16:19 +0200

    Updated Dtos

--- Social.API/Dtos/PostForReturnDto.cs
+++ Social.API/Dtos/PostForReturnDto.cs
@@ -7,7 +7,7 @@ namespace Social.API.Dtos
     {
         public int Id { get; set; }
         public string Text { get; set; }
-        public int UserId { get; set; }
+        public User User { get; set; }
         public ICollection<Like> Likes { get; set; }
         public ICollection<Comment> Comments { get; set; }
     }
