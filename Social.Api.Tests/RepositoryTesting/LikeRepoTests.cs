b83df3948d8fbb065f5ba522533ccbabe75d53a8
Pierre <pierre.nygard@gmail.com>
2020-05-18 16:35:34 +0200

    LikeRepoTests updated

--- Social.Api.Tests/RepositoryTesting/LikeRepoTests.cs
+++ Social.Api.Tests/RepositoryTesting/LikeRepoTests.cs
@@ -10,8 +10,17 @@ using System.Linq;
 
 namespace Social.Api.Tests
 {
-    public class LikeRepoTest
+    public class LikeRepoTests
     {
+        private readonly Mock<DataContext> _mockContext;
+        private readonly LikeRepository _mockRepo;
+
+        public LikeRepoTests()
+        {
+            _mockContext = new Mock<DataContext>();
+            _mockRepo = new LikeRepository(_mockContext.Object, Mock.Of<ILogger<LikeRepository>>());
+        }
+
         [Theory]
         [InlineData(500)]
         [InlineData(622)]
@@ -21,19 +30,15 @@ namespace Social.Api.Tests
         public async void GetLikesByPostId_LikesCount_ReturnsZero(int nonExistantPostId)
         {
             // Arrange
-            ILoggerFactory loggerFactory = new LoggerFactory();
-            ILogger<LikeRepository> logger = loggerFactory.CreateLogger<LikeRepository>();
             IList<Like> likes = new List<Like> {
                     GenerateFake.Like(),
                     GenerateFake.Like(),
                     GenerateFake.Like()
             };
-            var dataContext = new Mock<DataContext>();
-            dataContext.Setup(x => x.Likes).ReturnsDbSet(likes);
-            var likeRepository = new LikeRepository(dataContext.Object, logger);
+            _mockContext.Setup(x => x.Likes).ReturnsDbSet(likes);
 
             // Act
-            var like = await likeRepository.GetLikesByPostId(nonExistantPostId);
+            var like = await _mockRepo.GetLikesByPostId(nonExistantPostId);
 
             // Assert
             Assert.Equal(0, like.Count());
@@ -48,20 +53,16 @@ namespace Social.Api.Tests
         public async void GetLikeByPostId_LikesExist_ReturnsNotNull(int ExistantPostId)
         {
             // Arrange
-            ILoggerFactory loggerFactory = new LoggerFactory();
-            ILogger<LikeRepository> logger = loggerFactory.CreateLogger<LikeRepository>();
             IList<Like> likes = new List<Like> {
                     new Like(){Id = 1, User = GenerateFake.User(), Post = new Post(){Id = ExistantPostId}},
                     new Like(){Id = 2, User = GenerateFake.User(), Post = new Post(){Id = ExistantPostId}},
                     new Like(){Id = 3, User = GenerateFake.User(), Post = new Post(){Id = ExistantPostId}},
                     new Like(){Id = 4, User = GenerateFake.User(), Post = new Post(){Id = ExistantPostId}}
             };
-            var dataContext = new Mock<DataContext>();
-            dataContext.Setup(x => x.Likes).ReturnsDbSet(likes);
-            var likeRepository = new LikeRepository(dataContext.Object, logger);
+            _mockContext.Setup(x => x.Likes).ReturnsDbSet(likes);
 
             // Act
-            var likesList = await likeRepository.GetLikesByPostId(ExistantPostId);
+            var likesList = await _mockRepo.GetLikesByPostId(ExistantPostId);
 
             // Assert
             Assert.NotNull(likesList);
@@ -77,26 +78,17 @@ namespace Social.Api.Tests
         {
             //Arrange
             IList<Like> likes = new List<Like>();
-            
-            ILoggerFactory loggerFactory = new LoggerFactory();    
-            ILogger<LikeRepository> logger = loggerFactory.CreateLogger<LikeRepository>();
-
             for (int i = 0; i < expectedAmountLikes; i++)
             {
                 likes.Add(new Like(){Id = i+1, User = GenerateFake.User(), Post = new Post(){Id = postId}});
             }
-            var dataContext = new Mock<DataContext>();
-            dataContext.Setup(x => x.Likes).ReturnsDbSet(likes);
-            var likeRepository = new LikeRepository(dataContext.Object, logger);
+            _mockContext.Setup(x => x.Likes).ReturnsDbSet(likes);
 
             //Act
-            var likesFromRepo = await likeRepository.GetLikesByPostId(postId);
+            var likesFromRepo = await _mockRepo.GetLikesByPostId(postId);
 
             //Assert
             Assert.Equal(expectedAmountLikes, likesFromRepo.Count());
         }
-
-
-
     }
 }
