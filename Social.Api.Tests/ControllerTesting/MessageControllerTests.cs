7920e54c4dae081762bf09115795bb6a329c90e8
Pierre <pierre.nygard@gmail.com>
2020-05-25 13:18:10 +0200

    GetMessageById_ReturnsObjectResult

--- Social.Api.Tests/ControllerTesting/MessageControllerTests.cs
+++ Social.Api.Tests/ControllerTesting/MessageControllerTests.cs
@@ -1,9 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Social.API;
 using Social.API.Controllers;
+using Social.API.Models;
 using Social.API.Services;
+using Xunit;
 
 namespace Social.Api.Tests.ControllerTesting
 {
@@ -22,6 +27,20 @@ namespace Social.Api.Tests.ControllerTesting
             _mockMapper = new Mock<IMapper>();
             _urlHelper = new Mock<IUrlHelper>();
             _messageController = new MessageController(_mockRepo.Object, _mockMapper.Object, _urlHelper.Object);
+        }
+
+        [Fact]
+        public async Task GetMessageById_ReturnsObjectResult() {
+            // Arrange
+            Message message = GenerateFake.Message();
+            _mockRepo.Setup(repo => repo.GetMessageById(1))
+                .ReturnsAsync(message);
+            
+            // Act
+            var response = await _messageController.GetMessageById(1);
+
+            // Assert
+            Assert.IsAssignableFrom<ObjectResult>(response);
         }
     }
 }
