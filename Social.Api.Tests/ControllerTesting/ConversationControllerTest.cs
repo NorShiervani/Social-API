fa5d27faa0821077ff2b067ed854a75733c8664e
Andreas Brochs <55091646+AndreasBrochs@users.noreply.github.com>
2020-05-25 14:22:30 +0200

    mock enviorment for Conversation

---
+++ Social.Api.Tests/ControllerTesting/ConversationControllerTest.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Social.API;
+using Social.API.Controllers;
+using Social.API.Models;
+using Social.API.Services;
+using Xunit;
+
+namespace Social.Api.Tests
+{
+    public class ConversationControllerTest
+    {
+            private readonly Mock<DataContext> _mockContext;
+            private readonly Mock<IConversationRepository> _mockRepo;
+            private readonly Mock<IUrlHelper> _urlHelper;
+            private readonly Mock<IMapper> _mockMapper;
+            private readonly ConversationController _conversationController;
+
+             public ConversationControllerTest()
+            {
+                _mockContext = new Mock<DataContext>();
+                _mockRepo = new Mock<IConversationRepository>();
+                _mockMapper = new Mock<IMapper>();
+                _urlHelper = new Mock<IUrlHelper>();
+                _conversationController = new ConversationController(_mockRepo.Object, _mockMapper.Object, _urlHelper.Object);
+            }
+
+
+    }
+
+}
