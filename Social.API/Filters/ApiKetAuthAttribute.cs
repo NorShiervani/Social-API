5eef7c7f93a835eda813ce94b5a0abe6acd3d897
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-06-01 11:45:21 +0200

    added file for Filter
    Added a folder for filter and added ApiKeyAuthAttribute.cs

---
+++ Social.API/Filters/ApiKetAuthAttribute.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Social.API.Filters
+{
+    public class ApiKetAuthAttribute: Attribute, IAsyncActionFilter
+    {
+        private const string ApiKeyHeaderName= "ApiKey";
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {   
+           if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var potentialApiKey))
+           {
+               context.Result = new UnauthorizedResult();
+               return;
+           }
+
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apikey = configuration.GetValue<string>(key: "ApiKey");
+            
+            if(!apikey.Equals(potentialApiKey))
+            {
+               context.Result = new UnauthorizedResult();
+               return;
+            }
+
+            await next();
+        }
+    }
+}
